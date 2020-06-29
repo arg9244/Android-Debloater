e4a719929b1782bd999f4766ad28d96a8599ba55
arg9244 <arg9244@gmail.com>
2020-06-29 21:03:14 +0430

    Add files via upload

---
+++ pocophone f1.bat
@@ -0,0 +1,6 @@
+@echo off 
+set list=com.google.android.youtube com.google.android.inputmethod.latin com.mi.android.globalFileexplorer com.android.thememanager com.google.android.apps.maps com.xiaomi.mipicks com.miui.videoplayer com.miui.android.fashiongallery com.google.android.googlequicksearchbox com.google.android.tts com.facebook.appmanager com.xiaomi.account com.mipay.wallet.in com.xiaomi.payment com.google.ar.lens com.miui.mishare.connectivity com.mipay.wallet.id com.xiaomi.midrop com.miui.cloudservice com.miui.miservice com.xiaomi.glgm com.miui.screenrecorder com.miui.micloudsync com.android.fileexplorer com.facebook.services com.miui.huanji com.google.android.projection.gearhead com.facebook.system com.android.hotwordenrollment.xgoogle com.android.hotwordenrollment.okgoogle com.mi.globalbrowser com.miui.player
+(for %%a in (%list%) do ( 
+   adb\adb shell "pm uninstall -k --user 0 %%a" 
+))
+pause
